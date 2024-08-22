c6bba905521a5d8e57e048973a281ce134989f32
helldweller <helldweller@protonmail.com>
2024-08-23 05:35:30 +0700

    fix

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from PIL import Image
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 
 model_dir = './saved_model' # path to https://huggingface.co/Falconsai/nsfw_image_detection
-processor = AutoImageProcessor.from_pretrained("{model_dir}/preprocessor_config.json", local_files_only=True)
-model = AutoModelForImageClassification.from_pretrained("{model_dir}", local_files_only=True)
+image_dir = './images'
+processor = AutoImageProcessor.from_pretrained(f'{model_dir}/preprocessor_config.json', local_files_only=True)
+model = AutoModelForImageClassification.from_pretrained(f'{model_dir}', local_files_only=True)
 
 def predict(img_path):
-    img = Image.open(f'./{img_path}')
+    img = Image.open(f'{image_dir}/{img_path}')
     with torch.no_grad():
         inputs = processor(images=img, return_tensors="pt")
         outputs = model(**inputs)
@@ -24,7 +25,7 @@ def predict(img_path):
 t0 = time.time()
 imgs = ["usual1.jpg", "nsfw2.jpg", "nsfw1.jpg"]
 for x in imgs:
-  print(predict(x))
+  print(f'Image {x} is {predict(x)}')
 t1 = time.time()
 
 print(f'Time spent: {t1-t0}')
